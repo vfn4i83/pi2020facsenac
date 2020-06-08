e4c861b4b0781a70faf8b8502da078957e3db8e1
Rafael Carvalho e Lima <rafaelreg@outlook.com>
2020-06-08 15:47:07 -0300

    + back-end inicial

---
+++ PI2020_RafaelCL_Nicholas/src/main/java/back/senac/Pi2020RafaelClNicholasApplication.java
@@ -0,0 +1,13 @@
+package back.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi2020RafaelClNicholasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi2020RafaelClNicholasApplication.class, args);
+	}
+
+}
---
+++ PI2020_RafaelCL_Nicholas/src/test/java/back/senac/Pi2020RafaelClNicholasApplicationTests.java
@@ -0,0 +1,13 @@
+package back.senac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi2020RafaelClNicholasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
